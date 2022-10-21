32c9272e27dfed931dcfa0f25f92f150482c9dad
StormEkholm <rasmussh90skole@gmail.com>
2022-10-21 12:47:34 +0000

    product list low level

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,32 @@
-import './App.css';
+import data from './data';
 
 function App() {
   return (
     <div>
       <header>
-        <a href="/">
-          Learn React
-        </a>
+        <a href="/">Echoless</a>
       </header>
-      <main>List</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>{product.price} DKK</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,29 @@
+const data = {
+  products: [
+    {
+      name: "nike",
+      slug: "nike-sling",
+      category: "shirt",
+      image: "/images/fff.png",
+      price: 120,
+      countInStock: 10,
+      brand: "nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "hey hey hey",
+    },
+    {
+      name: "nike",
+      slug: "nike-sling2",
+      category: "shirt2",
+      image: "/images/fff.png",
+      price: 120,
+      countInStock: 10,
+      brand: "nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "hey hey hey2a",
+    },
+  ],
+};
+export default data;
